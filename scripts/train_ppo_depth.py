cdc67581161cde087c9161bf5fc63a4e570b5875
crislmfroes <cris.lima.froes@gmail.com>
2025-07-19 15:29:58 -0300

    Updates

--- scripts/train_ppo_depth.py
+++ scripts/train_ppo_depth.py
@@ -21,6 +21,8 @@ from mani_skill.utils import gym_utils
 from mani_skill.utils.wrappers.flatten import FlattenActionSpaceWrapper, FlattenRGBDObservationWrapper
 from mani_skill.utils.wrappers.record import RecordEpisode
 from mani_skill.vector.wrappers.gymnasium import ManiSkillVectorEnv
+import fbot_rl.envs
+
 
 @dataclass
 class Args:
